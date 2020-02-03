ea3ac8cb83907a5b829d649b5be65f4c64bfa9a4
BuildTools <toxicscopesgames@gmail.com>
2020-02-03 00:31:35 +0000

    Added subscribing/unsubscribing from events

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const fs = require("fs");
 const jsonfile = require("jsonfile");
+const common = require("./store/common.js");
 
 const prefix = config.prefix;
 
 client.on("ready", () =>{
     console.log("Bot Loaded. Nice.");
-    client.user.setPresence({ game: { name: 'In Development! C: 1.1.0 |D: 1.2.0', type: "Watching" }, status: 'idle' });
-    //client.user.setPresence({ game: { name: 'Version 1.1.0', type: "Watching" }, status: 'online' });
+    //client.user.setPresence({ game: { name: 'In Development! C: 1.1.0 |D: 1.2.0', type: "Watching" }, status: 'idle' });
+    client.user.setPresence({ game: { name: 'Version 1.2.0', type: "Watching" }, status: 'online' });
 
     client.commands = new Discord.Collection();
     const commandDirArray = fs.readdirSync("./commands");
@@ -18,7 +19,22 @@ client.on("ready", () =>{
         client.commands.set(commandFile.name, commandFile);
     });
 });
-
+/*==========================================================================================================================================================================================================================
+|| Unfinished. Do not un-comment.                                                                                                                                                                                         ||
+|| Issues: figure out a way to loop through each file for each event. May require re-factor of backend for events?                                                                                                        ||
+============================================================================================================================================================================================================================
+client.setInterval(() => {
+    const gEvents = require(`./store/guilds/${guild.id}/guildEvents.json`);
+    for(var event in gEvents){
+        var currTime = moment();
+        if(moment(event.timeDestined) > currTime){
+            event.members.forEach(member =>{
+                member.send(`Greetings! This is an automated reminder that the event titled **${event.title}** has started in the last 3 or so seconds.\nPlease make your way to the event! Thanks for using Chrono!**`);
+            });
+        }
+    }
+}, 3000);
+==========================================================================================================================================================================================================================*/
 client.on("message", (message) => {
     const guild = message.guild;
     const gConfig = require(`./store/guilds/${guild.id}/guildConfig.json`);
@@ -75,7 +91,7 @@ client.on("guildCreate", (guild) => {
             }
         }
 
-        jsonfile.writeFile(gConfFile, gConf, {spaces: 4}, err =>{
+        jsonfile.writeFileSync(gConfFile, gConf, {spaces: 4}, err =>{
             if(err){
                 return console.log(`[CHRONO] [guildCreate] [ERROR] - jsonfile failed to write to file ${gConfFile}`);
             }else{
@@ -85,7 +101,7 @@ client.on("guildCreate", (guild) => {
                     .setAuthor(`You added Chrono to ${guild.name}`)
                     .addField(`Welcome to the Chrono family, ${guild.owner.displayName}!`, "Thanks for adding me! Read below for some top tips :)")
                     .addField("Use the help command to get started!", "Just type `~help`.")
-                    .addField("Don't be afraid to ask for help!", "If you ever need assistance, just contact `Vex#1337`. If you find a bug, report it on `https://github.com/veraxonhd/chronos-bot`!")
+                    .addField("Don't be afraid to ask for help!", "If you ever need assistance, just contact `Vex#1337`.\nIf you find a bug, report it on `https://github.com/veraxonhd/chronos-bot`!")
                     .setTimestamp(new Date());
                 guild.owner.send({embed});
             }
@@ -94,13 +110,101 @@ client.on("guildCreate", (guild) => {
 
     if(!fs.existsSync(gEventFile)){ 
         var gEvents = {};
-        jsonfile.writeFile(gEventFile, gEvents, {spaces: 4}, err =>{
+        jsonfile.writeFileSync(gEventFile, gEvents, {spaces: 4}, err =>{
             if(err){
                 return console.log(`[CHRONO] [guildCreate] [ERROR] - jsonfile failed to write to file ${gEventFile}`);
             }else{
                 console.log(`[CHRONO] [guildCreate] ${dir} has been populated with default data successfully.`);
             }
         });
+    }
+});
+
+const events = {
+	MESSAGE_REACTION_ADD: 'messageReactionAdd',
+	MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
+};
+
+client.on('raw', async event => {
+	if (!events.hasOwnProperty(event.t)) return;
+
+	const { d: data } = event;
+	const user = client.users.get(data.user_id);
+	const channel = client.channels.get(data.channel_id) || await user.createDM();
+
+	if (channel.messages.has(data.message_id)) return;
+	const message = await channel.fetchMessage(data.message_id);
+
+	const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
+	const reaction = message.reactions.get(emojiKey);
+
+	client.emit(events[event.t], reaction, user);
+});
+
+client.on("messageReactionAdd", (messageReaction, user) =>{
+    console.log(`Reaction Add Triggered`);
+    const message = messageReaction.message;
+    const guild = message.guild;
+
+    const gEvents = require(`./store/guilds/${guild.id}/guildEvents.json`);
+
+    if(messageReaction.emoji.name == "⏰" && user.id != client.user.id){
+        for(var event in gEvents){
+            if(message.id == gEvents[event].msgID){
+                var eventID = gEvents[event].eventID;
+
+                if(gEvents[event].members.indexOf(user.id) != -1){
+                    return;
+                }
+
+                console.log(`[CHRONO] [main.messageReactionAdd] Member not in array - adding...`);
+                gEvents[eventID].members.push(user.id);
+
+                jsonfile.writeFileSync(`./store/guilds/${message.guild.id}/guildEvents.json`, gEvents, { spaces: 4 }, (err) => {
+                    if(err){
+                        return console.log(`[CHRONO] [main.messageReactionAdd] [ERROR] - jsonfile failed to write to file ${gEventFile} with new event subscriber data for event ${eventID}.`);
+                    }else{
+                        return console.log(`[CHRONO] [main.messageReactionAdd] Event ${eventID} saved with new event subscriber data.`);
+                    }
+                });
+            }
+        }
+    }else{
+        return;
+    }
+});
+
+client.on("messageReactionRemove", (messageReaction, user) =>{
+    console.log(`Reaction Remove Triggered`);
+    const message = messageReaction.message;
+    const guild = message.guild;
+
+    const gEvents = require(`./store/guilds/${guild.id}/guildEvents.json`);
+
+    if(messageReaction.emoji.name == "⏰" && user.id != client.user.id){
+        for(var event in gEvents){
+            if(message.id == gEvents[event].msgID){
+                var eventID = gEvents[event].eventID;
+
+                if(gEvents[event].members.indexOf(user.id) == -1){
+                    return;
+                }
+
+                console.log(`[CHRONO] [main.messageReactionAdd] Member already in array - removing...`);
+                var pos = gEvents[event].members.indexOf(user.id);
+                gEvents[eventID].members.splice(pos, 1);
+                
+                jsonfile.writeFileSync(`./store/guilds/${message.guild.id}/guildEvents.json`, gEvents, { spaces: 4 }, (err) => {
+                    if(err){
+                        return console.log(`[CHRONO] [main.messageReactionAdd] [ERROR] - jsonfile failed to write to file ${gEventFile} with new event subscriber data for event ${eventID}.`);
+                    }else{
+                        return console.log(`[CHRONO] [main.messageReactionAdd] Event ${eventID} saved with new event subscriber data.`);
+                    }
+                });
+            }
+        }
+    }else{
+        return;
     }
 });
 
